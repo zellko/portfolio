c1a452de9fa29d8a0ede333c581ef945735bf233
zellko <wyttenda@tutanota.com>
2022-12-04 16:39:10 +0100

    Added React Route & Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import { AnimationBG } from './Utils/AnimationBG';
+import Header from './Layouts/Header';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Header />
       <AnimationBG />
+      <Routes>
+        <Route path="/" />
+        <Route path="/project/:name" />
+        <Route path="/about" />
+        <Route path="/contact" />
+      </Routes>
     </div>
   );
 }
