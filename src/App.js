1784a1d6644405455203823e8c1ce93f28095667
zellko <wyttenda@tutanota.com>
2022-12-09 20:34:38 +0100

    Added Home component to App Routes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { AnimationBG } from './Utils/AnimationBG';
 import Header from './Layouts/Header';
+import Home from './Routes/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AnimationBG />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/project/:name" />
         <Route path="/about" />
         <Route path="/contact" />
       </Routes>
+      <AnimationBG />
     </div>
   );
 }
