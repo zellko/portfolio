f37e736049ae10e815cb9ec0269b2c71358ab288
zellko <wyttenda@tutanota.com>
2023-01-16 18:59:13 +0100

    Added contentText file
    contentText is an object which will contain all the texts of my portfolio.
    
    The goal is to make it easier to modifty text or add a project card just by modifing this file instead of modifying components.

---
+++ src/contentText.js
@@ -0,0 +1,33 @@
+const contentText = {
+  home: {
+    header1: "Hi! I'm Zellkoss.",
+    header2: 'I am a full stack Web fsdfsdfds.',
+  },
+  projects: [
+    {
+      projectName: 'My Portfolio',
+      projectText: 'Abcd',
+      projectTech: 'React, Three.js',
+      projectImgLink: '/Assets/Images/portfolio.png',
+      projectTechImgLink: ['/Assets/Images/react.png', '/Assets/Images/three.png'],
+    },
+    {
+      projectName: 'Web3 Project',
+      projectText: 'Abcd',
+      projectTech: 'React, Solidty, Ether.js',
+      projectImgLink: '/Assets/Images/pjtweb3.png',
+      projectTechImgLink: ['/Assets/Images/react.png', '/Assets/Images/solidty.png', '/Assets/Images/etherjs.png'],
+    },
+    {
+      projectName: '3rd Project',
+      projectText: 'Abcd',
+      projectTech: 'React',
+      projectImgLink: '/Assets/Images/pjt3.png',
+      projectTechImgLink: ['/Assets/Images/react.png'],
+    },
+  ],
+  About: {},
+  contact: {},
+};
+
+export default contentText;
