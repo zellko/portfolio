68045969d521bd4d946b281a24132c272d25d17f
zellko <wyttenda@tutanota.com>
2023-01-17 19:36:06 +0100

    Added component stucture

--- src/Components/PjtCard/PjtCard.js
+++ src/Components/PjtCard/PjtCard.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './Projects.css';
+import { Link } from 'react-router-dom';
+import './PjtCard.css';
 
-function PjtCard() {
+function PjtCard({ data }) {
   return (
     <article className="pjtCard">
-      Project Card
+      <div className="card-text-container">
+        <h1>{data.projectName}</h1>
+        <h2>{data.projectTech}</h2>
+      </div>
+      <Link to={`/projects/${data.projectLinkName}`} href={`/projects/${data.projectLinkName}`}>
+        <div className="card-img-container">
+          <img src={data.projectImgLink} alt={data.projectImgLink[1]} />
+          <div className="card-skill-container">
+            {data.projectTechImgLink.map((logoData) => (
+              <img src={logoData[0]} alt={logoData[1]} key={logoData[1]} />
+            ))}
+          </div>
+        </div>
+      </Link>
     </article>
   );
 }
