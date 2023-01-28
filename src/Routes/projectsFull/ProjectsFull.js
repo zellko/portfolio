8e31a70b4320fc195c43727f764ba64db3987c79
zellko <wyttenda@tutanota.com>
2023-01-28 21:36:08 +0100

    Fetch project data from URL Params an ContentText

--- src/Routes/projectsFull/ProjectsFull.js
+++ src/Routes/projectsFull/ProjectsFull.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import contentText from '../../contentText';
+
+const projectsData = contentText.projects;
 
 function ProjectsFull() {
+  const [data, setData] = useState({
+    projectName: 'Error, Project not found',
+  });
+
+  const { name } = useParams();
+
+  useEffect(() => {
+    // Fetch correct project Data and add it to "data" State
+    for (let index = 0; index < projectsData.length; index++) {
+      const project = projectsData[index];
+
+      if (project.projectLinkName === name) {
+        setData(project);
+      }
+    }
+  }, []);
+
   return (
     <main className="projects">
       <h1>
-        Project Full Description:
+        {data.projectName}
       </h1>
     </main>
   );
