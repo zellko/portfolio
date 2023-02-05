4266318e2f9dad45804d1218c327a4eafe8a22d4
zellko <wyttenda@tutanota.com>
2023-02-05 17:59:08 +0100

    Added component structure

--- src/Routes/projectsFull/ProjectsFull.js
+++ src/Routes/projectsFull/ProjectsFull.js
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import contentText from '../../contentText';
+import './ProjectsFull.css';
 
 const projectsData = contentText.projects;
 
 function ProjectsFull() {
   const [data, setData] = useState({
     projectName: 'Error, Project not found',
+    projectText: '',
+    projectTech: '',
+    projectImgLink: [''],
+    projectTechImgLink: [['', 'alt']],
+    projectLinkName: 'nan',
   });
 
   const { name } = useParams();
@@ -23,10 +29,20 @@ function ProjectsFull() {
   }, []);
 
   return (
-    <main className="projects">
-      <h1>
-        {data.projectName}
-      </h1>
+    <main className="projects-full">
+      <div className="card-text-container">
+        <h1>{data.projectName}</h1>
+        <h2>{data.projectText}</h2>
+      </div>
+
+      <div className="card-img-container">
+        <img src={data.projectImgLink} alt={data.projectImgLink[1]} />
+        <div className="card-skill-container">
+          {data.projectTechImgLink.map((logoData) => (
+            <img src={logoData[0]} alt={logoData[1]} key={logoData[1]} />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
