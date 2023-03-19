4c3414eab77148444c2eefc5b9f4b7cf351b3898
zellko <wyttenda@tutanota.com>
2023-03-19 17:13:54 +0100

    Updated content and eslint rules

--- src/contentText.js
+++ src/contentText.js
@@ -6,26 +6,29 @@ const contentText = {
   projects: [
     {
       projectName: 'My Portfolio',
-      projectText: 'Abcd',
+      projectText: `Nullam et consequat dolor. Quisque blandit sit amet nisi in imperdiet. 
+      Pellentesque scelerisque in nulla non laoreet. Vivamus elementum pretium ipsum ac porttitor. 
+      In eget arcu nec mauris eleifend tristique eu commodo tellus. 
+      Duis bibendum sollicitudin mi at dapibus.`,
       projectTech: 'React, Three.js',
-      projectImgLink: ['/Assets/Images/portfolio.png', 'Portfolio project ilustration'],
-      projectTechImgLink: [['/Assets/Images/react.png', 'React logo'], ['/Assets/Images/three.png', 'Three.js logo']],
+      projectImgLink: ['https://dummyimage.com/520x228/7a527a/ffffff', 'Portfolio project ilustration'],
+      projectTechImgLink: [['https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg', 'React logo'], ['https://raw.githubusercontent.com/mrdoob/three.js/38bf5f47a8c01a1d12d16a41b4097dc9ee31daad/files/icon.svg', 'Three.js logo']],
       projectLinkName: 'myportfolio',
     },
     {
       projectName: 'Web3 Project',
       projectText: 'Abcd',
       projectTech: 'React, Solidty, Ether.js',
-      projectImgLink: ['/Assets/Images/pjtweb3.png', 'Web3 project ilustration'],
-      projectTechImgLink: [['/Assets/Images/react.png', 'React logo'], ['/Assets/Images/solidity.png', 'Solidity logo'], ['/Assets/Images/etherjs.png', 'Ether.js logo']],
+      projectImgLink: ['https://dummyimage.com/520x228/7a527a/ffffff', 'Web3 project ilustration'],
+      projectTechImgLink: [['https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg', 'React logo'], ['/Assets/Images/solidity.png', 'Solidity logo'], ['/Assets/Images/etherjs.png', 'Ether.js logo']],
       projectLinkName: 'web3',
     },
     {
       projectName: '3rd Project',
       projectText: 'Abcd',
       projectTech: 'React',
-      projectImgLink: ['/Assets/Images/pjt3.png', '3rd project ilustration'],
-      projectTechImgLink: ['/Assets/Images/react.png', 'React logo'],
+      projectImgLink: ['https://dummyimage.com/520x228/7a527a/ffffff', '3rd project ilustration'],
+      projectTechImgLink: [['https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg', 'React logo']],
       projectLinkName: '3dproject',
     },
   ],
